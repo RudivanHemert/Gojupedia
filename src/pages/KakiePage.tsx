2bfb0c0323a5ad0f2c4ebc150e5e6e1aea8129fc
RudivanHemert <rudivanhemert@gmail.com>
2026-01-20 08:48:04 +0100

    update gekisai

--- src/pages/KakiePage.tsx
+++ src/pages/KakiePage.tsx
@@ -51,10 +51,10 @@ const KakiePage = () => {
             title: t('kakie.sections.close-combat.title'),
             icon: <ShieldAlert className="h-5 w-5 text-orange-500" />,
             subsections: [
-                { id: 'basic-qualities', title: t('kakie.sections.close-combat.basic-qualities'), path: '/kakie/close-combat/basic-qualities' },
-                { id: 'chiru-nu-chan-chan', title: t('kakie.sections.close-combat.chiru-nu-chan-chan'), path: '/kakie/close-combat/chiru-nu-chan-chan' },
-                { id: 'body-conditioning', title: t('kakie.sections.close-combat.body-conditioning'), path: '/kakie/close-combat/body-conditioning' },
-                { id: 'sanchin', title: t('kakie.sections.close-combat.sanchin'), path: '/kakie/close-combat/sanchin' }
+                { id: 'basic-qualities', title: t('kakie.sections.close-combat.basic-qualities.title'), path: '/kakie/close-combat/basic-qualities' },
+                { id: 'chiru-nu-chan-chan', title: t('kakie.sections.close-combat.chiru-nu-chan-chan.title'), path: '/kakie/close-combat/chiru-nu-chan-chan' },
+                { id: 'body-conditioning', title: t('kakie.sections.close-combat.body-conditioning.title'), path: '/kakie/close-combat/body-conditioning' },
+                { id: 'sanchin', title: t('kakie.sections.close-combat.sanchin.title'), path: '/kakie/close-combat/sanchin' }
             ]
         },
         {
@@ -62,11 +62,11 @@ const KakiePage = () => {
             title: t('kakie.sections.kiko.title'),
             icon: <Zap className="h-5 w-5 text-yellow-500" />,
             subsections: [
-                { id: 'mind-projection', title: t('kakie.sections.kiko.mind-projection'), path: '/kakie/kiko/mind-projection' },
-                { id: 'sensitivity', title: t('kakie.sections.kiko.sensitivity'), path: '/kakie/kiko/sensitivity' },
-                { id: 'muscular-tension', title: t('kakie.sections.kiko.muscular-tension'), path: '/kakie/kiko/muscular-tension' },
-                { id: 'regulating-breathing', title: t('kakie.sections.kiko.regulating-breathing'), path: '/kakie/kiko/regulating-breathing' },
-                { id: 'tension-relaxation', title: t('kakie.sections.kiko.tension-relaxation'), path: '/kakie/kiko/tension-relaxation' }
+                { id: 'mind-projection', title: t('kakie.sections.kiko.mind-projection.title'), path: '/kakie/kiko/mind-projection' },
+                { id: 'sensitivity', title: t('kakie.sections.kiko.sensitivity.title'), path: '/kakie/kiko/sensitivity' },
+                { id: 'muscular-tension', title: t('kakie.sections.kiko.muscular-tension.title'), path: '/kakie/kiko/muscular-tension' },
+                { id: 'regulating-breathing', title: t('kakie.sections.kiko.regulating-breathing.title'), path: '/kakie/kiko/regulating-breathing' },
+                { id: 'tension-relaxation', title: t('kakie.sections.kiko.tension-relaxation.title'), path: '/kakie/kiko/tension-relaxation' }
             ]
         },
         {
@@ -74,11 +74,11 @@ const KakiePage = () => {
             title: t('kakie.sections.basic-abilities.title'),
             icon: <Target className="h-5 w-5 text-indigo-500" />,
             subsections: [
-                { id: 'control-absorb-deflect-evade', title: t('kakie.sections.basic-abilities.control-absorb-deflect-evade'), path: '/kakie/basic-abilities/control-absorb-deflect-evade' },
-                { id: 'control-muchimi', title: t('kakie.sections.basic-abilities.control-muchimi'), path: '/kakie/basic-abilities/control-muchimi' },
-                { id: 'absorb', title: t('kakie.sections.basic-abilities.absorb'), path: '/kakie/basic-abilities/absorb' },
-                { id: 'deflection', title: t('kakie.sections.basic-abilities.deflection'), path: '/kakie/basic-abilities/deflection' },
-                { id: 'evasion', title: t('kakie.sections.basic-abilities.evasion'), path: '/kakie/basic-abilities/evasion' }
+                { id: 'control-absorb-deflect-evade', title: t('kakie.sections.basic-abilities.control-absorb-deflect-evade.title'), path: '/kakie/basic-abilities/control-absorb-deflect-evade' },
+                { id: 'control-muchimi', title: t('kakie.sections.basic-abilities.control-muchimi.title'), path: '/kakie/basic-abilities/control-muchimi' },
+                { id: 'absorb', title: t('kakie.sections.basic-abilities.absorb.title'), path: '/kakie/basic-abilities/absorb' },
+                { id: 'deflection', title: t('kakie.sections.basic-abilities.deflection.title'), path: '/kakie/basic-abilities/deflection' },
+                { id: 'evasion', title: t('kakie.sections.basic-abilities.evasion.title'), path: '/kakie/basic-abilities/evasion' }
             ]
         },
         {
@@ -86,12 +86,12 @@ const KakiePage = () => {
             title: t('kakie.sections.techniques.title'),
             icon: <Activity className="h-5 w-5 text-rose-500" />,
             subsections: [
-                { id: 'grabbing', title: t('kakie.sections.techniques.grabbing'), path: '/kakie/techniques/grabbing' },
-                { id: 'opening-closing', title: t('kakie.sections.techniques.opening-closing'), path: '/kakie/techniques/opening-closing' },
-                { id: 'pushing-pulling', title: t('kakie.sections.techniques.pushing-pulling'), path: '/kakie/techniques/pushing-pulling' },
-                { id: 'go-techniques', title: t('kakie.sections.techniques.go-techniques'), path: '/kakie/techniques/go-techniques' },
-                { id: 'ju-techniques-tuite', title: t('kakie.sections.techniques.ju-techniques-tuite'), path: '/kakie/techniques/ju-techniques-tuite' },
-                { id: 'ju-techniques-kyusho', title: t('kakie.sections.techniques.ju-techniques-kyusho'), path: '/kakie/techniques/ju-techniques-kyusho' }
+                { id: 'grabbing', title: t('kakie.sections.techniques.grabbing.title'), path: '/kakie/techniques/grabbing' },
+                { id: 'opening-closing', title: t('kakie.sections.techniques.opening-closing.title'), path: '/kakie/techniques/opening-closing' },
+                { id: 'pushing-pulling', title: t('kakie.sections.techniques.pushing-pulling.title'), path: '/kakie/techniques/pushing-pulling' },
+                { id: 'go-techniques', title: t('kakie.sections.techniques.go-techniques.title'), path: '/kakie/techniques/go-techniques' },
+                { id: 'ju-techniques-tuite', title: t('kakie.sections.techniques.ju-techniques-tuite.title'), path: '/kakie/techniques/ju-techniques-tuite' },
+                { id: 'ju-techniques-kyusho', title: t('kakie.sections.techniques.ju-techniques-kyusho.title'), path: '/kakie/techniques/ju-techniques-kyusho' }
             ]
         },
         {
@@ -99,9 +99,9 @@ const KakiePage = () => {
             title: t('kakie.sections.basic-exercises.title'),
             icon: <Dumbbell className="h-5 w-5 text-cyan-500" />,
             subsections: [
-                { id: 'swallow-spit', title: t('kakie.sections.basic-exercises.swallow-spit'), path: '/kakie/basic-exercises/swallow-spit' },
-                { id: 'circle-deflect', title: t('kakie.sections.basic-exercises.circle-deflect'), path: '/kakie/basic-exercises/circle-deflect' },
-                { id: 'kake-uke', title: t('kakie.sections.basic-exercises.kake-uke'), path: '/kakie/basic-exercises/kake-uke' }
+                { id: 'swallow-spit', title: t('kakie.sections.basic-exercises.swallow-spit.title'), path: '/kakie/basic-exercises/swallow-spit' },
+                { id: 'circle-deflect', title: t('kakie.sections.basic-exercises.circle-deflect.title'), path: '/kakie/basic-exercises/circle-deflect' },
+                { id: 'kake-uke', title: t('kakie.sections.basic-exercises.kake-uke.title'), path: '/kakie/basic-exercises/kake-uke' }
             ]
         },
         {
@@ -109,14 +109,14 @@ const KakiePage = () => {
             title: t('kakie.sections.points-of-attention.title'),
             icon: <Layers className="h-5 w-5 text-amber-500" />,
             subsections: [
-                { id: 'posture', title: t('kakie.sections.points-of-attention.posture'), path: '/kakie/points-of-attention/posture' },
-                { id: 'grounding', title: t('kakie.sections.points-of-attention.grounding'), path: '/kakie/points-of-attention/grounding' },
-                { id: 'rooting', title: t('kakie.sections.points-of-attention.rooting'), path: '/kakie/points-of-attention/rooting' },
-                { id: 'centring', title: t('kakie.sections.points-of-attention.centring'), path: '/kakie/points-of-attention/centring' },
-                { id: 'generating-power', title: t('kakie.sections.points-of-attention.generating-power'), path: '/kakie/points-of-attention/generating-power' },
-                { id: 'combining-forces', title: t('kakie.sections.points-of-attention.combining-forces'), path: '/kakie/points-of-attention/combining-forces' },
-                { id: 'regulating-breathing', title: t('kakie.sections.points-of-attention.regulating-breathing'), path: '/kakie/points-of-attention/regulating-breathing' },
-                { id: 'regulating-ki', title: t('kakie.sections.points-of-attention.regulating-ki'), path: '/kakie/points-of-attention/regulating-ki' }
+                { id: 'posture', title: t('kakie.sections.points-of-attention.posture.title'), path: '/kakie/points-of-attention/posture' },
+                { id: 'grounding', title: t('kakie.sections.points-of-attention.grounding.title'), path: '/kakie/points-of-attention/grounding' },
+                { id: 'rooting', title: t('kakie.sections.points-of-attention.rooting.title'), path: '/kakie/points-of-attention/rooting' },
+                { id: 'centring', title: t('kakie.sections.points-of-attention.centring.title'), path: '/kakie/points-of-attention/centring' },
+                { id: 'generating-power', title: t('kakie.sections.points-of-attention.generating-power.title'), path: '/kakie/points-of-attention/generating-power' },
+                { id: 'combining-forces', title: t('kakie.sections.points-of-attention.combining-forces.title'), path: '/kakie/points-of-attention/combining-forces' },
+                { id: 'regulating-breathing', title: t('kakie.sections.points-of-attention.regulating-breathing.title'), path: '/kakie/points-of-attention/regulating-breathing' },
+                { id: 'regulating-ki', title: t('kakie.sections.points-of-attention.regulating-ki.title'), path: '/kakie/points-of-attention/regulating-ki' }
             ]
         }
     ];
